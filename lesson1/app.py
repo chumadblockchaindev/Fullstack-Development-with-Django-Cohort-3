17e81fda783c545d03ddb131ac926a6ccadea043
Chukwuma Ezenobi <chukwumaizu@icloud.com>
2025-09-15 11:20:13 +0100

    first commit

---
+++ lesson1/app.py
@@ -0,0 +1,55 @@
+# print statement, variables, assignment and maths operators, comments
+
+# A print statement outputs data to the console
+
+# print("Hello, World!")
+# print("Welcome to Python programming.")
+
+# backend_students = ['Uchenna', 'Odili', 'Leonard', 'Gideon'] # A list of backend students
+
+# for student in backend_students:
+#     print(student)  
+
+# Variables
+# A variable is a label for storing data in the computer's memory.
+
+# number1 = 10  
+# string1 = "Hello, Python!"
+# boolean1 = True
+
+
+# Comments
+# Comments are notes in the code that are ignored by the interpreter.
+""" This is a \
+    multiline comment example \
+        more comments here \
+            even more comments here"""
+
+import math
+# Maths Operators
+# # +, -, *, /, %, **, //
+
+a = 15
+b = 4
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
+print(a % b)  # Modulus
+print(a ** b) # Exponentiation
+print(a // b) # Floor Division
+print(math.sqrt(16)) # Square root
+print(math.pow(a, b)) # Power function
+print(math.pi) # Pi constant
+
+# Assignmnet: Stydy Assignment Operators
+# Assignment Operators
+# +=	x += 5	x = x + 5	Adds the right operand to the left and assigns the result.
+# -=	x -= 3	x = x - 3	Subtracts the right operand from the left and assigns the result.
+# *=	x *= 2	x = x * 2	Multiplies the left operand by the right and assigns the result.
+# /=	x /= 4	x = x / 4	Divides the left operand by the right and assigns the result.
+# %=	x %= 3	x = x % 3	Finds the remainder of the division and assigns the result.
+
+# Data Types
+# Integers, floats, strings, booleans, and more. Input/output, print formatting
+
